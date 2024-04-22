ea9750b566d67272a8b99fb8bf7dae2e06b620bb
Mateusz Brzeziński <mateusz.p.brzezinski@gmail.com>
2024-04-22 16:44:00 +0200

    actual db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,19 @@ import {db} from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-    "https://utfs.io/f/c59a076d-d7fc-4abf-978e-1233c4ddd8ba-c3f0v5.jpg",
-    "https://utfs.io/f/db6a622d-57b7-4911-b741-b8591cc76156-xdlva9.jpg",
-    "https://utfs.io/f/d9f50511-1640-4fc8-a11c-cbfc87230262-xdlva6.jpg",
-    "https://utfs.io/f/044aa41a-8510-4648-9e84-57d600acdbd6-c3f0w0.jpg"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url
-}));
-
 export default async function HomePage() {
-    const images = await db.query.posts.findMany();
-    console.log(images);
+    const images = await db.query.images.findMany({
+        orderBy: (model, {desc}) => desc(model.id),
+    });
 
     return (
         <main className="">
             <div className="flex flex-wrap gap-4">
-                {images.map((image) => (<div key={image.id}>{image.name}</div>))}
-                {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-                    <div key={image.id + "-" + index} className="w-48">
-                        <img src={image.url} alt="" className="w-full"/>
+                {[...images, ...images, ...images].map((image, index) => (
+                    <div key={image.id + "-" + index} className="w-48 flex flex-col">
+                        <img src={image.url}/>
+                        <div>{image.name}</div>
+
                     </div>
                 ))
                 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,13 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import {sql} from "drizzle-orm";
 import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
+    index,
+    pgTableCreator,
+    serial,
+    timestamp,
+    varchar,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -18,17 +18,18 @@ import {
  */
 export const createTable = pgTableCreator((name) => `theo-2024_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+export const images = createTable(
+    "image",
+    {
+        id: serial("id").primaryKey(),
+        name: varchar("name", {length: 256}).notNull(),
+        url: varchar("url", {length: 1024}).notNull(),
+        createdAt: timestamp("created_at")
+            .default(sql`CURRENT_TIMESTAMP`)
+            .notNull(),
+        updatedAt: timestamp("updatedAt"),
+    },
+    (example) => ({
+        nameIndex: index("name_idx").on(example.name),
+    })
 );
