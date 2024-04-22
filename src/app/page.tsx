5ba3f7976ce9f659e67e5991ee4ba2c402faefac
Mateusz Brzeziński <mateusz.p.brzezinski@gmail.com>
2024-04-22 15:51:37 +0200

    scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+    "https://utfs.io/f/c59a076d-d7fc-4abf-978e-1233c4ddd8ba-c3f0v5.jpg",
+    "https://utfs.io/f/db6a622d-57b7-4911-b741-b8591cc76156-xdlva9.jpg",
+    "https://utfs.io/f/d9f50511-1640-4fc8-a11c-cbfc87230262-xdlva6.jpg",
+    "https://utfs.io/f/044aa41a-8510-4648-9e84-57d600acdbd6-c3f0w0.jpg"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url
+}));
+
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello, gallery in progress
-    </main>
-  );
+    return (
+        <main className="">
+            <div className="flex flex-wrap gap-4">{
+                [ ...mockImages, ...mockImages, ...mockImages].map(image => (
+                    <div key={image.id} className="w-48">
+                        <img src={image.url} alt="" className="w-full"/>
+                    </div>
+                ))
+            }
+            </div>
+        </main>
+    );
 }
---
+++ src/components/TopNav.tsx
@@ -0,0 +1,8 @@
+export default function TopNav() {
+    return (
+        <nav className={"flex items-center justify-between w-full p-4 text-xl font-semibold border-b"}>
+            <div>Gallery</div>
+            <div>Sign In</div>
+        </nav>
+    )
+}
