31a834d97b56e2648e86932b18e8c617fb9b745b
Mateusz Brzeziński <mateusz.p.brzezinski@gmail.com>
2024-04-23 12:49:39 +0200

    add next-image

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -1,13 +1,14 @@
 import {getMyImages} from "~/server/queries";
+import Image from "next/image";
 
 export default async function Images() {
     const images = await getMyImages();
 
     return (
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap justify-center gap-4">
             {images.map((image) => (
-                <div key={image.id} className="w-48 flex flex-col">
-                    <img src={image.url} alt="iamge"/>
+                <div key={image.id} className="w-48 h-48 flex flex-col">
+                    <Image src={image.url} style={{objectFit: "contain"}} alt={image.name} width={192} height={192}/>
                     <div>{image.name}</div>
                 </div>
             ))}
