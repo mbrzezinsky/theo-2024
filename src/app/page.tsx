6b7f68bd880e3882940cae64a0e5b38b26861871
Mateusz Brzeziński <mateusz.p.brzezinski@gmail.com>
2024-04-24 23:52:09 +0200

    wip on parallel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
-import {db} from "~/server/db";
-import {SignedIn, SignedOut} from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Images from "~/app/_components/images";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-
-    return (
-        <main className="">
-            <SignedOut>
-                <div className = "w-full text-center h-full text-2xl">
-                    Please sign in to view this page
-                </div>
-            </SignedOut>
-            <SignedIn>
-                <Images/>
-            </SignedIn>
-        </main>
-    );
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in to view this page
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  );
 }
