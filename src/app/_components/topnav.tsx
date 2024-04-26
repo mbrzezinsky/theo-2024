bc9b5c48932451ecc0d6152c4e03b58d5f957a2a
Mateusz Brzeziński <mateusz.p.brzezinski@gmail.com>
2024-04-26 08:40:41 +0200

    setup shadui

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,27 +1,23 @@
-"use client";
-
-import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
-import {UploadButton} from "~/utils/uploadthing";
-import {useRouter} from "next/navigation";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SimpleUploadButton } from "~/app/_components/simple-upload-button";
 
 export default function Topnav() {
-    const router = useRouter();
-
-    return (
-        <nav className={"flex items-center justify-between w-full p-4 text-xl font-semibold border-b"}>
-            <div>Gallery</div>
-            <div className={"flex flex-row gap-4"}>
-                <SignedOut>
-                    <SignInButton/>
-                </SignedOut>
-                <SignedIn>
-                    <UploadButton endpoint={"imageUploader"}
-                                  onClientUploadComplete={() => {
-                                      router.refresh();
-                                  }}/>
-                    <UserButton/>
-                </SignedIn>
-            </div>
-        </nav>
-    )
+  return (
+    <nav
+      className={
+        "flex w-full items-center justify-between border-b p-4 text-xl font-semibold"
+      }
+    >
+      <div>Gallery</div>
+      <div className={"flex flex-row items-center gap-4"}>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <SimpleUploadButton />
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
 }
