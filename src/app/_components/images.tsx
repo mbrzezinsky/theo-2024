54a6db5e7efbc40dc4398f8cad896c1393bb1a2b
Mateusz Brzeziński <mateusz.p.brzezinski@gmail.com>
2024-04-29 12:38:00 +0200

    fix flex layout of main page iamges with flex-basis

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -8,7 +8,7 @@ export default async function Images() {
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
-        <div key={image.id} className="flex h-48 w-48 flex-col">
+        <div key={image.id} className="flex basis-48 flex-col">
           <Link href={`/img/${image.id}`}>
             <Image
               src={image.url}
